39932733519787e17eb14d7aff5e9dd176e726b6
unknown <anamika362890@gmail.com>
2022-11-08 02:15:12 +0600

    loginForm

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Other/Sign/Login";
+import Signup from "../../Pages/Other/Sign/Signup";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +12,16 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/signup",
+                element: <Signup></Signup>
             }
+
 
 
         ]
